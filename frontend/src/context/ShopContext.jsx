c0da987807e1a42949eb41bf4f119709b81a321f
vixwha <viswaprakash0001@gmail.com>
2024-12-26 20:28:52 +0530

    day 4

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast} from "react-toastify"
 
 export const ShopContext = createContext();
 
@@ -8,10 +9,44 @@ const ShopContextProvider = (props) => {
     const currency = '$';
     const devivery_fee = 10;
     const [search,setSearch] = useState('');
-    const [showSearch,setShowSearch] = useState(false)
+    const [showSearch,setShowSearch] = useState(false);
+    const [cartItems,setCartItems] = useState({});
+
+
+    const addToCart = async (itemId,size) => {
+        if (!size) {
+            toast.error('Select Product Size');
+            return
+            }
+
+        let CartData = structuredClone(cartItems);
+
+        if (CartData[itemId]) {
+            if (CartData[itemId][size]) {
+                CartData[itemId][size] += 1;
+                
+            }
+            else{
+                CartData[itemId][size] = 1;
+            }
+            
+        }
+        else{
+            CartData[itemId] ={};
+            CartData[itemId][size]= 1;
+        }
+        setCartItems(CartData);
+
+    }
+    useEffect(()=>{
+       console.log(cartItems);
+       
+    },[cartItems])
+
 
     const value ={
-          products,currency,devivery_fee,search,setSearch,showSearch,setShowSearch
+          products,currency,devivery_fee,search,setSearch,showSearch,setShowSearch,
+          cartItems,addToCart
     }
     return (
         <ShopContext.Provider value={value}>
