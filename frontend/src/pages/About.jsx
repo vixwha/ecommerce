c29d535cf07634e54d98faed4c39a91b354450f5
vixwha <viswaprakash0001@gmail.com>
2025-01-02 22:07:10 +0530

    day 06

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import Title from "../components/Title";
+import { assets } from '../assets/assets';
+import './About.css'
+import NewsletterBox from"../components/NewsletterBox";
 
 const About = () => {
   return (
     <div>
-      About
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img className='w-full md:max-w-[450px]:' src={assets.about_img} alt="" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At eaque fugiat debitis? Amet aut velit quia, fugit nostrum facere obcaecati, nihil ipsum dolore et, magni placeat id fuga necessitatibus exercitationem.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic non a temporibus enim voluptatum quod placeat esse nulla quibusdam autem sed quaerat magni, culpa accusamus. Minus fugiat eius officiis officia?</p>
+          <b className='text-gray-600'>Our Mission</b>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ab officiis excepturi! Veniam magnam deleniti labore est qui mollitia, nisi porro eum commodi id, quod rem, atque suscipit quas repellendus!</p>
+        </div>
+        
+      </div>
+      <div className="text-xl py-4">
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+      <div className="flex felc-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic laudantium laborum ad sed, labore tempora.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-20 flex flex-col gap-5">
+          <b>Convenice:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic laudantium laborum ad sed, labore tempora..</p>
+        </div>
+        <div className="border px-10 md:px-16 py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic laudantium laborum ad sed, labore tempora.</p>
+        </div>
+        
+      </div>
+      <NewsletterBox/>
       
     </div>
   )
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import Title from "../components/Title";
+import { assets } from '../assets/assets';
+import NewsletterBox from '../components/NewsletterBox';
 
 const Contact = () => {
   return (
     <div>
-      Contact
-    </div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={'CONTACT'} text2={'US'}/>
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <div className=" inline-block justify-center items-start gap-6">
+          <p className='font-semibold text-xl text-gray-600 mb-5'>Our Store</p>
+          <p className='text-gray-500 mb-2'>54709 Willms station <br />suite,washinton,US</p>
+          <p className='text-gray-500'>Tel: (415) 555-0123 <br />Email: admin@forever.com</p>
+          <p className='mt-20 mb-3 font-semibold text-xl text-gray-600'>Careers at Forever</p>
+          <p className='text-gray-500'>Learn more about out teams and job openings.</p>
+          <button className='mt-7 border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          
+          </div>
+          
+        </div>
+        <NewsletterBox/>
+      </div>
+    
   )
 }
 
