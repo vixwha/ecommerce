d7fee5fd1a4675d54c852bdda4d063e708c3524e
vixwha <viswaprakash0001@gmail.com>
2025-02-13 13:08:08 +0530

    modified 1

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ import orderRouter from './routes/orderRoute.js'
 //App config
 
 const app = express()
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5001;
 connectDB()
 connectCloudinary()
 
